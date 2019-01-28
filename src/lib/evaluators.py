112834e6077a4e2a6f5ae31162b49020132ec2ac
crux <saogmuas@gmail.com>
2019-01-28 04:51:47 -0300

    Adding test folder with notebooks. Changeing the logic of evaluation. Instead of using numpy array only lists in order to allow variable target linkages.

--- src/lib/evaluators.py
+++ src/lib/evaluators.py
@@ -43,16 +43,25 @@ class SeedTargetRanking(Evaluator):
         score_matrix = algorithms.Propagator.label_propagator(network, seeds_matrix, self.alpha, tol=self.tol,
                                                               max_iter=self.max_iter, exponent=self.laplacian_exponent)
         score_matrix = score_matrix * (seeds_matrix == 0)  # drop from ranking all seeds
-        score_matrix = np.array([score_matrix[targets, i] for i, targets in enumerate(targets_list.T)]).T # target scores
 
-        ranking = score_matrix.shape[0]-score_matrix.argsort(axis=0).argsort(axis=0)
+        ranking = []
+        for i, targets in enumerate(targets_list):
+            ranking.append((score_matrix.shape[0] - score_matrix[targets, i].argsort().argsort()).tolist())
+
+        # score_matrix = np.array([score_matrix[targets, i] for i, targets in enumerate(targets_list.T)]).T # target scores
+        # ranking = score_matrix.shape[0]-score_matrix.argsort(axis=0).argsort(axis=0)
         return ranking
 
-    def __init__(self, ranking_to_value_function, seeds_matrix, targets_list, alpha, tol=1e-08, max_iter=100, exponent=-0.5):
+    def __init__(self, ranking_to_value_function, seeds_matrix, targets_list, true_targets_list, alpha, tol=1e-08, max_iter=100, exponent=-0.5):
         self.alpha = alpha
         self.tol = tol
         self.max_iter = max_iter
         self.laplacian_exponent = exponent
+
+        # true_targets is a mask, should be the same shape
+        self.y_true = []
+        for targets, true_targets in zip(targets_list, true_targets_list):
+            self.y_true += [1 if target in true_targets else 0 for target in targets]
 
         def evaluator_function(network):
             return ranking_to_value_function(self.ranking_function(network, seeds_matrix, targets_list))
@@ -61,7 +70,7 @@ class SeedTargetRanking(Evaluator):
 
 
 class AUROClinkage(SeedTargetRanking):
-    def ranking_to_value(self, target_rankings, true_targets):
+    def ranking_to_value(self, target_rankings_list):
         """
         TODO: warning! roc_auc_score is not clear what does if max_fpr not 1
         :param target_rankings:
@@ -69,9 +78,14 @@ class AUROClinkage(SeedTargetRanking):
         :return:
         """
 
-        assert true_targets.shape == target_rankings.shape # true_targets is a mask, should be the same shape
+        y_score = [-ranking for target_rankings in target_rankings_list for ranking in target_rankings]
+
+        fpr, tpr, thresholds = metrics.roc_curve(y_true=self.y_true,
+                                                 y_score=y_score)
+
         # warning! -terget_rankings because roc_auc goes from minus to plus
-        fpr, tpr, thresholds = metrics.roc_curve(y_true=true_targets.ravel(), y_score=-target_rankings.ravel())
+        # fpr, tpr, thresholds = metrics.roc_curve(y_true=[element for tt in true_targets for element in tt],
+        #                                          y_score=[-element for tr in target_rankings for element in tr])
         auc_unnormalized = np.trapz(x=fpr[fpr <= self.max_fpr], y=tpr[fpr <= self.max_fpr])
         max_auc = self.max_fpr
         min_auc = max_auc ** 2 / 2
@@ -79,7 +93,7 @@ class AUROClinkage(SeedTargetRanking):
         return (1 + (auc_unnormalized - min_auc) / (max_auc - min_auc)) / 2
         # return metrics.roc_auc_score(y_true=true_targets.ravel(), y_score=-target_rankings.ravel(), max_fpr=self.max_fpr)
 
-    def __init__(self, seeds_matrix, targets_list, true_targets, alpha, tol=1e-08, max_iter=100,
+    def __init__(self, seeds_matrix, targets_list, true_targets_list, alpha, tol=1e-08, max_iter=100,
                  laplacian_exponent=-0.5, max_fpr=1):
         """
 
@@ -95,10 +109,10 @@ class AUROClinkage(SeedTargetRanking):
         self.metric_name = "AUROC_{}".format(max_fpr)
         self.max_fpr = max_fpr
 
-        def ranking_to_value_function(target_rankings):
-            return self.ranking_to_value(target_rankings, true_targets)
+        def ranking_to_value_function(target_rankings_list):
+            return self.ranking_to_value(target_rankings_list)
 
-        SeedTargetRanking.__init__(self, ranking_to_value_function, seeds_matrix, targets_list, alpha, tol, max_iter,
+        SeedTargetRanking.__init__(self, ranking_to_value_function, seeds_matrix, targets_list, true_targets_list, alpha, tol, max_iter,
                                    laplacian_exponent)
 
 
@@ -126,8 +140,10 @@ if __name__ == "__main__":
     seeds_matrix = np.eye(N)
     targets_list = np.array([np.roll(np.arange(N), -n)[1:] for n in range(N)]).T
     targets_list = targets_list[:n_targets, :]
-    true_targets = np.zeros(targets_list.shape)
-    true_targets[1, :] = 1
+    # true_targets = np.zeros(targets_list.shape)
+    # true_targets[1, :] = 1
+    true_targets = targets_list[0].reshape((-1, 1))
+
     alpha = 0.2
     laplacian_exponent = -0.5
 
